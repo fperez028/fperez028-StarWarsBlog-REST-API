0c1d26400b7e832766be72150b665c3e896d6a77
Frank Perez <21010185+fperez028@users.noreply.github.com>
2025-08-01 17:04:05 +0000

    MIGRATE to migrate, pipfile python version changed to 3.10

--- src/app.py
+++ src/app.py
@@ -20,7 +20,7 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db)
+migrate = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
